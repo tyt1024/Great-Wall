fe943ece90f7eae6cec7f83d8386e03240fd50ec
Sharon <sharontam1024@gmail.com>
2016-01-18 12:17:50 -0800

    lab in class
    add pages and done about us page

---
+++ about.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<link href="./css/style.css" rel="stylesheet" type="text/css"/>
+<link href="./css/responsive.css" rel="stylesheet" type="text/css"/>
+  <!-- View -->
+<title>Great Wall - About Us</title>
+    <navReplace metal:use-macro="./templating/nav.xhtml/nav_bar">
+    </navReplace>
+  <body>
+
+
+
+
+  <headReplace metal:use-macro="/templating/head.xhtml/meta_head">
+  </headReplace>
+
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="./js/main.js"></script>
+    <div id="aboutdiv">
+    <h1 class="aboutT">ABOUT GREAT WALL</h1>
+    <p class="aboutP">Great Wall Chinese Restaurant has been serving the local community for over 25 years and has a strong reputation for preparing the finest Cantonese and Szechuan cuisine within the Greater Vancouver area. Our clean environment and delightful dishes have earned thousands of patrons and repeat customers over the years Our fast, efficient and courteous service will make your visit an enjoyable experience. Come for lunch or dinner or select from our extensive take-out menu. We also provide fast FREE delivery within a 5 km radius.
+Reasonably priced, Burnaby Palace Restaurant has become a mainstay for business lunch and fine family style dining.
+Try our delicious food and elegant ambiance. You won’t be disappointed. Service matters a lot to Burnaby Palace Restaurant and we are committed to providing the highest level of excellence in customer service. Special care is always taken whether it’s preparing meals or making deliveries.
+*Reservations are recommended. For delivery, please place orders 1/2 hour before closing.</p>
+    <img class="aboutimg" src="img/Interior-design-ideas-of-Chinese-restaurant-room.jpg" width="1216" height="642" alt=""/> 
+    </div>
+  <footerReplace metal:use-macro="/templating/footer.xhtml/page_footer">
+    </footerReplace>
+
+  </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<link href="./css/style.css" rel="stylesheet" type="text/css"/>
+<link href="./css/responsive.css" rel="stylesheet" type="text/css"/>
+  <!-- View -->
+<title>Great Wall - Menu</title>
+    <navReplace metal:use-macro="./templating/nav.xhtml/nav_bar">
+    </navReplace>
+  <body>
+
+
+
+
+  <headReplace metal:use-macro="/templating/head.xhtml/meta_head">
+  </headReplace>
+
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="./js/main.js"></script>
+
+    <footerReplace metal:use-macro="/templating/footer.xhtml/page_footer">
+    </footerReplace>
+
+  </body>
+</html>
