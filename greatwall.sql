79ab38d07c5c2011033cea5e03f83453bda7a507
ChloeGuan <guandaxin@guandaxntekimbp.wifi.bcit.ca>
2016-02-05 14:09:01 -0800

    database

--- greatwall.sql
+++ greatwall.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7
+-- version 4.4.1.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Generation Time: Jan 29, 2016 at 09:47 PM
--- Server version: 5.5.41-log
--- PHP Version: 7.0.0
+-- Host: localhost
+-- Generation Time: Feb 05, 2016 at 11:08 PM
+-- Server version: 5.5.42
+-- PHP Version: 5.6.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,15 +26,15 @@ SET time_zone = "+00:00";
 -- Table structure for table `cart`
 --
 
-CREATE TABLE IF NOT EXISTS `cart` (
-`id` int(11) NOT NULL,
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
   `food_id` int(11) NOT NULL,
-  `img_url` varchar(300) NOT NULL,
+  `url` varchar(300) NOT NULL,
   `price` varchar(300) NOT NULL,
   `status` varchar(300) NOT NULL,
   `total_num` int(5) NOT NULL,
   `avail_num` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -42,16 +42,24 @@ CREATE TABLE IF NOT EXISTS `cart` (
 -- Table structure for table `product`
 --
 
-CREATE TABLE IF NOT EXISTS `product` (
-`id` int(11) NOT NULL,
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
   `name` varchar(500) NOT NULL,
   `type` varchar(500) NOT NULL,
   `description` varchar(500) NOT NULL,
-  `img_url` varchar(500) NOT NULL,
+  `url` varchar(500) NOT NULL,
   `price` varchar(500) NOT NULL,
   `total_num` int(5) NOT NULL,
-  `avail_num` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `avail_num` int(5) NOT NULL,
+  `SKU` varchar(500) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `type`, `description`, `url`, `price`, `total_num`, `avail_num`, `SKU`) VALUES
+(1, 'Fried Rice', 'Rice', 'Prawn, Steamed Rice, Green Bean, Ham, Eggs', 'img/fried_rice.jpg', '8.99', 5, 5, 'sk-2843y');
 
 -- --------------------------------------------------------
 
@@ -59,7 +67,7 @@ CREATE TABLE IF NOT EXISTS `product` (
 -- Table structure for table `product_cart`
 --
 
-CREATE TABLE IF NOT EXISTS `product_cart` (
+CREATE TABLE `product_cart` (
   `user_id` int(11) NOT NULL,
   `food_id` int(11) NOT NULL,
   `cart_id` int(11) NOT NULL,
@@ -72,11 +80,18 @@ CREATE TABLE IF NOT EXISTS `product_cart` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-`id` int(11) NOT NULL,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `userbane` varchar(300) NOT NULL,
   `password` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `userbane`, `password`) VALUES
+(1, 'chloeguan', '12345');
 
 --
 -- Indexes for dumped tables
@@ -86,25 +101,27 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Indexes for table `cart`
 --
 ALTER TABLE `cart`
- ADD PRIMARY KEY (`id`), ADD KEY `food_id` (`food_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `food_id` (`food_id`);
 
 --
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `product_cart`
 --
 ALTER TABLE `product_cart`
- ADD PRIMARY KEY (`user_id`), ADD KEY `food_id` (`food_id`,`cart_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `food_id` (`food_id`,`cart_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -114,17 +131,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
